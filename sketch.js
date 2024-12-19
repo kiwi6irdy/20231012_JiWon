6e887cd5adfba8dd22213b6f8d12cf9396c30a1b
kiwi6irdy <wblee691919@icloud.com>
2024-12-19 15:55:13 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,12 @@ var amp;
 
 let img;
 let vol = 0.5;
-let particles = []; // 입자들을 담을 배열
+let particles = []; 
+
+let jumpPositions = [0, 0, 0, 0, 0]; 
+let currentJump = 0; 
+let totalDuration1 = 172; 
+let totalDuration2 = 227; 
 
 function preload() {
   img = loadImage("images.jpg");
@@ -37,12 +42,18 @@ function setup() {
 
   sliderRate = createSlider(0, 1.5, 1, 0.01);
 
-  // 입자 화면 
   for (let i = 0; i < 30; i++) {
     particles.push(new Particle());
   }
 
   amp = new p5.Amplitude();
+
+  
+  jumpPositions[0] = 0;
+  jumpPositions[1] = totalDuration1 / 5;
+  jumpPositions[2] = totalDuration1 * 2 / 5;
+  jumpPositions[3] = totalDuration1 * 3 / 5;
+  jumpPositions[4] = totalDuration1 * 4 / 5;
 }
 
 function togglePlayingSong1() {
@@ -70,18 +81,19 @@ function togglePlayingSong2() {
 }
 
 function jumpSong() {
-  let len = song1.isPlaying() ? song1.duration() : song2.duration();
-  let t = random(len);
-  console.log('Jump to:', t);
   if (song1.isPlaying()) {
-    song1.jump(t);
+    
+    song1.jump(jumpPositions[currentJump]);
+    currentJump = (currentJump + 1) % jumpPositions.length; 
   } else if (song2.isPlaying()) {
-    song2.jump(t);
+    
+    song2.jump(jumpPositions[currentJump]);
+    currentJump = (currentJump + 1) % jumpPositions.length; 
   }
 }
 
 function volumeUp() {
-  vol = constrain(vol + 0.2, 0, 1);
+  vol = constrain(vol + 0.2, 0, 1); 
   console.log('Volume Up:', vol);
   song1.setVolume(vol);
   song2.setVolume(vol);
@@ -96,22 +108,20 @@ function volumeDown() {
 
 function draw() {
   background(0); 
-  image(img, 0, 0, 640, 640);
-
-  var volLevel = amp.getLevel(); // 현재 볼륨 기기
+  image(img, 0, 0, 640, 640); 
+  var volLevel = amp.getLevel(); 
   var diam = map(volLevel, 0, 0.3, 10, 200);
 
-  // 각 입자 업데이트, 그리기
+
   particles.forEach(p => {
-    p.update(volLevel);
-    p.display();
+    p.update(volLevel);  
+    p.display();         
   });
 
   song1.rate(sliderRate.value());
   song2.rate(sliderRate.value());
 }
 
-// 입자 클래스 정의
 class Particle {
   constructor() {
     this.x = random(width);
@@ -119,16 +129,14 @@ class Particle {
     this.size = random(5, 15);
     this.speed = random(1, 3);
     this.angle = random(TWO_PI);
-    this.color = color(random(50, 100), random(50, 100), random(50, 100), 150); 
-  }
+    this.color = color(random(50, 100), random(50, 100), random(50, 100), 150); }
 
   update(level) {
-    // 입자 이동
+    // 입자의 이동
     this.x += this.speed * cos(this.angle);
     this.y += this.speed * sin(this.angle);
     this.size = map(level, 0, 0.3, 5, 50);
 
-    // 화면 벗어나면 입자의 위치를 재설정
     if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
       this.x = random(width);
       this.y = random(height);
@@ -138,6 +146,5 @@ class Particle {
   display() {
     noStroke();
     fill(this.color);
-    ellipse(this.x, this.y, this.size); // 입자 그리기
-  }
+    ellipse(this.x, this.y, this.size); }
 }
